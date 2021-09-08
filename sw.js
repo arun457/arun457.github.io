e5b5e5e0ed9e30f62afe393e02ce2604ac3b1e9b
Arun Shrestha <oldoldphotos123456789@gmail.com>
2021-09-08 19:26:03 +0545

    added pwa

---
+++ sw.js
@@ -0,0 +1,56 @@
+const staticCacheName = 'site-static-v1';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/',
+    '/css/',
+    '/img/',
+    '/images/',
+    '/libs/',
+    '/links/',
+    '/scss/',
+    '/favicon.ico',
+    '/gulpfile.js',
+    '/manifest.json',
+    '/package-lock.json',
+    '/package.json',
+
+
+];
+
+// install event
+self.addEventListener('install', evt => {
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request).then(fetchRes => {
+                return caches.open(staticCacheName).then(cache => {
+                    cache.put(evt.request.url, fetchRes.clone());
+                    return fetchRes;
+                })
+            });
+        })
+    );
+});
+
